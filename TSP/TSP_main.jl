490b0df190e9246d8b8f9d683645b1fa9674baa2
Ameliouse <sun_amelie@hotmail.com>
2024-01-07 00:22:47 +0100

    ringStarNonCompact
    pb mincut

--- TSP/TSP_main.jl
+++ TSP/TSP_main.jl
@@ -7,7 +7,6 @@ include("TSP_heuristique.jl")
 include("TSP_meta_heuristique.jl")
 include("TSP_PLNE_MTZ.jl")
 include("TSP_BandC.jl")
-include("../ringStar/compact.jl")
 
 function Resoud_TSP(filename)
 	
@@ -45,31 +44,31 @@ function Resoud_TSP(filename)
 	
 	# Launch compact PLNE and visualization for the stable set problem
 
-    @time @CPUtime S_MTZ=PLNE_TSP_MTZ(I) # on le résout
+    # @time @CPUtime S_MTZ=PLNE_TSP_MTZ(I) # on le résout
  
-	val_MTZ=Compute_value_TSP(I, S_MTZ)
-	println("Solution MTZ :S=",S_MTZ)
-	println("Valeur: ",val_MTZ)
-	println()
+	# val_MTZ=Compute_value_TSP(I, S_MTZ)
+	# println("Solution MTZ :S=",S_MTZ)
+	# println("Valeur: ",val_MTZ)
+	# println()
 	
-	filename_MTZ = replace(filename, ".tsp" => "_MTZ")
+	# filename_MTZ = replace(filename, ".tsp" => "_MTZ")
 
-	WritePdf_visualization_solution_ordre(I,S_MTZ,filename_MTZ) # on fait le pdf
+	# WritePdf_visualization_solution_ordre(I,S_MTZ,filename_MTZ) # on fait le pdf
 	
 	
     #    # Launch compact PLNE and visualization for the stable set problem
 
- 	# @time @CPUtime S_BC=BandC_TSP(I) # on le résout
+ 	@time @CPUtime S_BC=BandC_TSP(I) # on le résout
  
-	# val_BC=Compute_value_TSP(I, S_BC)
-	# println("Solution BandC :S=",S_BC)
-	# println("Valeur: ",val_BC)
-	# println()
+	val_BC=Compute_value_TSP(I, S_BC)
+	println("Solution BandC :S=",S_BC)
+	println("Valeur: ",val_BC)
+	println()
 
 	
-	# filename_BC = replace(filename, ".tsp" => "_BandC")
+	filename_BC = replace(filename, ".tsp" => "_BandC")
 
-	# WritePdf_visualization_solution_ordre(I,S_BC,filename_BC) # on fait le pdf
+	WritePdf_visualization_solution_ordre(I,S_BC,filename_BC) # on fait le pdf
 
 
 
--- ringStar/ringStar_main.jl
+++ ringStar/ringStar_main.jl
@@ -3,26 +3,41 @@ using CPLEX
 using CPUTime
 
 include("../TSP/TSP_IO.jl")
-include("compact.jl")
+include("ringStar_compact.jl")
+include("ringStar_ncompact.jl")
 
 function Resoud_ringStar(filename)
-	
+
+	p = 5
+
 	I = Read_undirected_TSP(filename)
 	println(I)
 	
 	filename_inst = replace(filename, ".tsp" => "_inst")
     WritePdf_visualization_TSP(I, filename_inst)
 
-	@time @CPUtime stations, affectations, cycle =ring_star_compact(I, 5) # on le résout
- 
+	@time @CPUtime stations, affectations, cycle =ring_star_compact(I, p) # on le résout
+
 	val_RingStarCompact=Compute_value_RingStar(I, cycle)
 	println("Solution Ring Star compact :S=",cycle)
 	println("Valeur: ",val_RingStarCompact)
 	println()
-	
+
 	filename_RingStar = replace(filename, ".tsp" => "_RingStar")
 
 	WritePdf_visualization_solution_ordre(I,cycle,filename_RingStar) # on fait le pdf
+
+	@time @CPUtime stations, affectations, cycle =ring_star_ncompact(I, p) # on le résout
+ 
+	val_RingStarCompact=Compute_value_RingStar(I, cycle)
+	println("Solution Ring Star ncompact :S=",cycle)
+	println("Valeur: ",val_RingStarCompact)
+	println()
+	
+	filename_RingStar = replace(filename, ".tsp" => "_nRingStar")
+
+	WritePdf_visualization_solution_ordre(I,cycle,filename_RingStar) # on fait le pdf
+
 
 
 end
